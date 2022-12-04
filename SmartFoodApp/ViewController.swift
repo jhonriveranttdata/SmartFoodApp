97177c1d8217d785b3a0bd0f514eb2a43b0a0fb0
Jhon Bryan Rivera Mino <jrivermi@emeal.nttdata.com>
2022-12-04 11:53:42 -0500

    Diseño inicial

---
+++ SmartFoodApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  homeViewController.swift
+//  SmartFoodApp
+//
+//  Created by Jhon Bryan Rivera Mino on 25/11/22.
+//
+
+import UIKit
+
+class homeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
